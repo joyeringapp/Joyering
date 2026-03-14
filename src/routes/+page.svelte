9d5a41553dc1d60327efb10a13d34bda7960881e
Ulrika <joyeringapp@gmail.com>
2026-03-14 17:28:39 +0100

    Remove version from legal section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -891,7 +891,6 @@
 
           <div class="settings-section legal-section">
             <p class="legal-brand">Joyering™</p>
-            <p class="legal-version">Version 1.0</p>
             <p class="legal-copy">© 2026 Ulrika Torquato</p>
             <p class="legal-copy">All rights reserved.</p>
           </div>>
