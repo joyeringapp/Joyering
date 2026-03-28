21da86a01b01cf306fb8938b06408216c67e25f7
Ulrika <joyeringapp@gmail.com>
2026-03-28 15:42:04 +0100

    add assetlinks for android app links

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.joyering.app',
+  appName: 'Joyering',
+  webDir: 'build',
+  // @ts-ignore
+  appScheme: 'joyering'
+};
+
+export default config;
